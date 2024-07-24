63d894185498076c60ca8ba2790541df5adc8716
Gustavo Junior dos Santos <gustavojuniordos@hotmail.com>
2024-07-24 14:56:27 -0300

    Criado o super usuário do django-admin
    close #7

--- mysite/base/admin.py
+++ mysite/base/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
-#
-# # Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ("author", 'title', "text", "created_date", "published_date")
