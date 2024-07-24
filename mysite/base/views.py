19cc7c06b75e319c3c4865e40c1e5d952f13c625
Gustavo Junior dos Santos <gustavojuniordos@hotmail.com>
2024-07-24 20:28:30 -0300

    Trazendo dados do backend para frontend
    close #15

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from mysite.base.models import Post
+
+
 # from django.http import HttpResponse
 
 
 def post_list(request):
-    return render(request, 'base/post_list.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'base/post_list.html', {"posts": posts})
